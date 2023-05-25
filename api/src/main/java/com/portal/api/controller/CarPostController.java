773aec2a7716079d44196b889e54e7fad4045d71
robertpaivadf <robertpaiva.df@gmail.com>
2023-05-25 18:03:25 -0300

    Criando as classes controllers

---
+++ api/src/main/java/com/portal/api/controller/CarPostController.java
@@ -0,0 +1,36 @@
+package com.portal.api.controller;
+
+import com.portal.api.dto.CarPostDTO;
+import com.portal.api.service.CarPostStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/car")
+public class CarPostController {
+
+    @Autowired
+    private CarPostStoreService carPostStoreService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<List<CarPostDTO>> getCarSales(){
+        return ResponseEntity.status(HttpStatus.FOUND).body(carPostStoreService.getCarForSales());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity changeCarSale(@RequestBody CarPostDTO carPostDTO, @PathVariable("id") String id){
+        carPostStoreService.changeCarForSale(carPostDTO,id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCarSale(@PathVariable("id") String id){
+        carPostStoreService.removeCarForSale(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
